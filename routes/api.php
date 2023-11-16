ca5abab7a11bab7e49b152b2d4e71993ae1240ce
Ramon <ramonyepezhdz@gmail.com>
2023-11-16 11:03:16 -0600

    Agregado: rutas de API

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\VentaController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -24,6 +28,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 // Route::prefix('v1')->group(function () {
 // });
 Route::post('/users/login', [AuthController::class, 'login']);
+Route::post('/users', [UserController::class, 'store']);
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/users/logout', [AuthController::class, 'logout']);
+    Route::get('/users', [UserController::class, 'index']);
+    Route::get('/users/{id}', [UserController::class, 'show']);
+    Route::put('/users/{id}', [UserController::class, 'update']);
+    Route::delete('/users/{id}', [UserController::class, 'destroy']);
+    Route::apiResource('/products', ProductController::class);
+    Route::apiResource('/sells', VentaController::class);
 });
